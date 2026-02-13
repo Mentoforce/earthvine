138e83e7c1c42cb82b1987ad7c7235bb19b834ac
Vanshika <vanshika.mentoforce@gmail.com>
2026-02-13 11:45:08 +0530

    residential section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import ResidentialSection from "@/components/ResidentialSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <ResidentialSection />
     </>
   );
 }
